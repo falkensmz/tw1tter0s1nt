6bf13d746f85debb04e2c9f09d206c31caa2a9b3
c0m3t-k2 <83426553+c0m3t-k2@users.noreply.github.com>
2021-06-27 20:17:44 +0300

    twosint 2.0.3 - New modules
    nearVan and cl0udchas3r modules have been added. Type info nearVan/cl0udchas3r for more information!
    
    The update has been officially released . Not a beta nor an alpha release. If the modules aren't working, it's a development error so please contact me in case that happens! Peace <3

--- twosint.py
+++ twosint.py
@@ -4,6 +4,7 @@
 
 # project code name : coyote
 
+
 import twint
 from datetime import datetime
 import os
@@ -110,7 +111,31 @@ def soloInvestigation():
     
     twint.run.Lookup(c)
 
-# project code name : coyote
+def nearVan():
+    print()
+    c = twint.Config()
+    c.Username = username
+    
+    city = input("twosint~# Enter the city -> ")
+
+    c.Near = city
+
+    twint.run.Search(c)
+
+def cl0udchas3r():
+    print()
+    c = twint.Config()
+    c.Username = username
+    min_likes = int(input("twosint~# Minimum likes -> "))
+    min_retweets = int(input("twosint~# Minimum retweets -> "))
+    min_replies = int(input("twosint~# Minimum replies -> "))
+
+    c.Min_likes = min_likes
+    c.Min_retweets = min_retweets
+    c.Min_replies = min_replies
+
+    twint.run.Search(c)
+
 
 def infoUsernameSearch():
     print(r"""
@@ -164,6 +189,34 @@ def infoSoloInvestigation():
     target by all means. It scrapes their bio, current tweets,
     followers , following etc. """)
 
+def infoNearVan():
+    print(r"""
+    nearVan - module number 7
+    This module allows you to scrape tweets that ONLY 
+    have a specific city that you choose in them. For instance
+    you choose New York, and the tweet(s) MUST have been tweeted
+    in New York. The way this works is that it uses Geo-Coded tweets.
+    So the target MUST have specified the city in the tweet(s)
+    """)
+
+def infoLink3r():
+    print(r"""
+    link3r - module number 8
+    This module allows you to ONLY scrape tweets that have
+    links in them. The way it works is that it only searches for
+    tweets with links for instance 'look at this cool website my
+    friend has developed -> http://example.com/skdw_?'
+    """)
+
+def infoCl0udChas3r():
+    print(r"""
+    cl0udchas3r - module number 9
+    This module allows you to specify minimum likes,
+    retweets and replies and with the information you
+    provide it, it will try to gather the tweets that
+    meet your requirements!
+    """)
+
 def introduction():
     choice = input("twosint~# ")
     if choice == 'help':
@@ -197,7 +250,9 @@ def introduction():
         4 - followHunter                [get a lot of information on target's followers]
         5 - whoHunter                   [who is your target following]
         6 - soloInvestigation           [get current stats and more on your tareget]
-        
+        7 - nearVan                     [scrape tweets near a city you specify]
+        8 - link3R                      [scrape tweets that have links in them] {NOT RELEASED}
+        9 - cl0udchas3r                 [scrape tweets with minimum likes/retweets/replies]
         """)
         print()
         introduction()
@@ -219,13 +274,24 @@ def introduction():
     if choice == 'run 4' or choice == 'run followHunter':
         print()
         followHunter()
-    if choice == '5' or choice == 'run whoHunter':
+    if choice == 'run 5' or choice == 'run whoHunter':
         print()
         whoHunter()
     if choice == 'run 6' or choice == 'run soloInvestigation':
         print()
         print("twosint~# Running the soloInvestigation module ... ")
         soloInvestigation()
+    if choice == 'run 7' or choice == 'run nearVan':
+        print()
+        nearVan()
+    if choice == 'run 8' or choice == 'run link3r':
+        print()
+        print("twosint~# This module is still in development")
+        print()
+    if choice == 'run 9' or choice == 'run cl0udchas3r':
+        print()
+        cl0udchas3r()
+        print()
     if choice == 'info 0' or choice == 'info usernameSearch':
     	print()
     	infoUsernameSearch()
@@ -254,9 +320,19 @@ def introduction():
     	print()
     	infoSoloInvestigation()
     	print()
+    if choice == 'info 7' or choice == 'info nearVan':
+        print()
+        infoNearVan()
+        print()
+    if choice == 'info 8' or choice == 'info link3r':
+        print()
+        print("twosint~# This module is still in development ... ")
+        print()
+    if choice == 'info 9' or choice == 'info cl0udchas3r':
+        print()
+        infoCl0udChas3r()
+        print()
     else:
         introduction()
 
 introduction()
-
-# project code name : coyote
