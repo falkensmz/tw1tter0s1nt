7455b9aec8328554d716bd871fe3621790299915
falkensmz <83426553+falkensmz@users.noreply.github.com>
2022-02-24 14:55:16 +0200

    twosint 2.0.5 - full release
    in this release, I have finally finished the link3r module . there were many issues with this module that I needed some time to fix. also, there have been made slight aesthetic improvements which might help you organize your investigation better.

--- twosint.py
+++ twosint.py
@@ -1,8 +1,8 @@
 # complete tool with cmd line interface for more advanced users
 
-# author : falkensmz
+# author : falkensmaze (c0m3t-k2)
 
-# project code name : impish scrper
+# project code name : coyote
 
 
 import twint
@@ -11,21 +11,20 @@ import os
 import sys
 
 print()
-print(r"""                                                                                                                
- _________________  _____                   _____             ______   ____  _____   ______   _________________ 
-/                 \|\    \   _____     ____|\    \        ___|\     \ |    ||\    \ |\     \ /                 \
-\______     ______/| |    | /    /|   /     /\    \      |    |\     \|    | \\    \| \     \\______     ______/
-   \( /    /  )/   \/     / |    ||  /     /  \    \     |    |/____/||    |  \|    \  \     |  \( /    /  )/   
-    ' |   |   '    /     /_  \   \/ |     |    |    | ___|    \|   | ||    |   |     \  |    |   ' |   |   '    
-      |   |       |     // \  \   \ |     |    |    ||    \    \___|/ |    |   |      \ |    |     |   |        
-     /   //       |    |/   \ |    ||\     \  /    /||    |\     \    |    |   |    |\ \|    |    /   //        
-    /___//        |\ ___/\   \|   /|| \_____\/____/ ||\ ___\|_____|   |____|   |____||\_____/|   /___//         
-   |`   |         | |   | \______/ | \ |    ||    | /| |    |     |   |    |   |    |/ \|   ||  |`   |          
-   |____|          \|___|/\ |    | |  \|____||____|/  \|____|_____|   |____|   |____|   |___|/  |____|          
-     \(               \(   \|____|/      \(    )/        \(    )/       \(       \(       )/      \(            
-      '                '      )/          '    '          '    '         '        '       '        '            
-                              '                                                                                 
-twosint - Twitter OSiNT framework .- version 2.0.5 beta 
+print(r"""
+
+  :::::::::::     :::       :::       ::::::::       ::::::::       :::::::::::       ::::    :::   ::::::::::: 
+     :+:         :+:       :+:      :+:    :+:     :+:    :+:          :+:           :+:+:   :+:       :+:      
+    +:+         +:+       +:+      +:+    +:+     +:+                 +:+           :+:+:+  +:+       +:+       
+   +#+         +#+  +:+  +#+      +#+    +:+     +#++:++#++          +#+           +#+ +:+ +#+       +#+        
+  +#+         +#+ +#+#+ +#+      +#+    +#+            +#+          +#+           +#+  +#+#+#       +#+         
+ #+#          #+#+# #+#+#       #+#    #+#     #+#    #+#          #+#           #+#   #+#+#       #+#          
+###           ###   ###         ########       ########       ###########       ###    ####       ###           
+
+
+
+                                                                
+twosint - Twitter OSiNT framework .- version 2.0.5 Full Release 
 Credits - @ faleknsmz
 
 """)
@@ -167,6 +166,20 @@ def geoLocater():
     except Exception:
         print("twosint~# [-] twint was unable to grab the user's location")
 
+def link3r():
+    print()
+    try:
+        option_first = input("twosint~# Do you want to include or exclude links from tweets? -> ")
+        option = option_first.lower()
+
+        c = twint.Config()
+        c.Username = username
+        c.Links = option
+
+        twint.run.Search(c)
+    
+    except Exception:
+        print("twosint~# [-] An unknown error has occured!")
 
 def infoUsernameSearch():
     print(r"""
@@ -233,10 +246,9 @@ def infoNearVan():
 def infoLink3r():
     print(r"""
     link3r - module number 8
-    This module allows you to ONLY scrape tweets that have
-    links in them. The way it works is that it only searches for
-    tweets with links for instance 'look at this cool website my
-    friend has developed -> http://example.com/skdw_?'
+    This module allows you to 'include' or 'exclude' links
+    from a users' tweets. All you need to do is type 'include'
+    or 'exclude' when asked and twint will do the work.
     """)
 
 def infoCl0udChas3r():
@@ -263,6 +275,7 @@ def infoGeoLocater():
     location.
     """)
 
+
 #def socials():
     print(r"""
     Discord -> falkensmaze#0001
@@ -271,141 +284,144 @@ def infoGeoLocater():
     twosint email -> twosintcontact@protonmail.com
     """)
 def introduction():
-    choice = input("twosint~# ")
-    if choice == 'help':
-        print(r"""
-                    twosint - version 2.0.5 Beta Release
+    try:
+
+        choice = input("twosint~# ")
+        if choice == 'help':
+            print(r"""
+                        twosint - version 2.0.5 Full Release
+                
+                version                     - displays current version
+                clear                       - clears the screen
+                exit                        - exit
+                modulebomb                  - shows all modules
+                username                    - shows current set username
+                run [module name / module number]  - runs the chosen module
+                info [module name / module number] - get more information on a specified module
+            """)
+            introduction()
+        if choice == 'version':
+            print("twosint~# twosint - version 2.0.5 Full Release ")
+        if choice == 'clear':
+            os.system("clear")
+        if choice == 'exit':
+            print("twosint~# Exitting ...")
+            quit()
+        if choice == 'modulebomb':
+            print()
+            print(r"""
             
-            version                     - displays current version
-            clear                       - clears the screen
-            exit                        - exit
-            modulebomb                  - shows all modules
-            username                    - shows current set username
-            run [module name / module number]  - runs the chosen module
-            info [module name / module number] - get more information on a specified module
-        """)
-        introduction()
-    if choice == 'version':
-        print("twosint~# twosint - version 2.0.5 Beta ")
-    if choice == 'clear':
-        os.system("clear")
-    if choice == 'exit':
-        print("twosint~# Exitting ...")
-        quit()
-    if choice == 'modulebomb':
-        print()
-        print(r"""
-        
-        0 - usernameSearch              [get usernames of your target's followers]
-        1 - keyHunter                   [get tweets that only have your chosen keyword in them]
-        2 - mailHunter                  [search for potential emails]
-        3 - numHunter                   [search for potential phone numbers]
-        4 - followHunter                [get a lot of information on target's followers]
-        5 - whoHunter                   [who is your target following]
-        6 - soloInvestigation           [get current stats and more on your tareget]
-        7 - nearVan                     [scrape tweets near a city you specify]
-        8 - link3R                      [scrape tweets that have links in them] {NOT RELEASED YET}
-        9 - cl0udchas3r                 [scrape tweets with minimum likes/retweets/replies]
-        10 - shadowHunter               [researches shadow banned accounts]
-        11 - geoLocater                 [attempts to grab user's location]
-        """)
-        print()
-        introduction()
-    if choice == 'socials':
-        socials()
-    if choice == 'username':
-        print("twosint~#", username)
-        introduction()
-    if choice == 'run usernameSearch' or choice == 'run 0':
-        print()
-        usernameSearch()
-    if choice == 'run 1' or choice == 'run keyHunter':
-        print()
-        keyHunter()
-    if choice == 'run 2' or choice == 'run mailHunter':
-        print()
-        mailHunter()
-    if choice == 'run 3' or choice == 'run numHunter':
-        print()
-        numHunter()
-    if choice == 'run 4' or choice == 'run followHunter':
-        print()
-        followHunter()
-    if choice == 'run 5' or choice == 'run whoHunter':
-        print()
-        whoHunter()
-    if choice == 'run 6' or choice == 'run soloInvestigation':
-        print()
-        print("twosint~# Running the soloInvestigation module ... ")
-        soloInvestigation()
-    if choice == 'run 7' or choice == 'run nearVan':
-        print()
-        nearVan()
-    if choice == 'run 8' or choice == 'run link3r':
-        print()
-        print("twosint~# This module is still in development")
-        print()
-    if choice == 'run 9' or choice == 'run cl0udchas3r':
-        print()
-        cl0udchas3r()
-        print()
-    if choice == 'run 10' or choice == 'run shadowHunter':
-        print()
-        shadowHunter()
-        print()
-    if choice == 'run 11' or choice == 'run geoLocater':
-        print()
-        geoLocater()
-        print()
-    if choice == 'info 0' or choice == 'info usernameSearch':
-    	print()
-    	infoUsernameSearch()
-    	print()
-    if choice == 'info 1' or choice == 'info keyHunter':
-    	print()
-    	infoKeyHunter()
-    	print()
-    if choice == 'info 2' or choice == 'info mailHunter':
-    	print()
-    	infoMailHunter()
-    	print()
-    if choice == 'info 3' or choice == 'info numHunter':
-    	print()
-    	infoNumHunter()
-    	print()
-    if choice == 'info 4' or choice == 'info followHunter':
-    	print()
-    	infoFollowHunter()
-    	print()
-    if choice == 'info 5' or choice == 'info whoHunter':
-    	print()
-    	infoWhoHunter()
-    	print()
-    if choice == 'info 6' or choice == 'info soloInvestigation':
-    	print()
-    	infoSoloInvestigation()
-    	print()
-    if choice == 'info 7' or choice == 'info nearVan':
-        print()
-        infoNearVan()
-        print()
-    if choice == 'info 8' or choice == 'info link3r':
-        print()
-        print("twosint~# This module is still in development ... ")
-        print()
-    if choice == 'info 9' or choice == 'info cl0udchas3r':
-        print()
-        infoCl0udChas3r()
-        print()
-    if choice == 'info 10' or choice == 'info shadowHunter':
-        print()
-        infoShadowHunter()
-        print()
-    if choice == 'info 11' or choice == 'info geoLocater':
-        print()
-        infoGeoLocater()
-        print()
-    else:
-        introduction()
+            0 - usernameSearch              [get usernames of your target's followers]
+            1 - keyHunter                   [get tweets that only have your chosen keyword in them]
+            2 - mailHunter                  [search for potential emails]
+            3 - numHunter                   [search for potential phone numbers]
+            4 - followHunter                [get a lot of information on target's followers]
+            5 - whoHunter                   [who is your target following]
+            6 - soloInvestigation           [get current stats and more on your tareget]
+            7 - nearVan                     [scrape tweets near a city you specify]
+            8 - link3R                      [scrape tweets that have links in them] {NOT RELEASED YET}
+            9 - cl0udchas3r                 [scrape tweets with minimum likes/retweets/replies]
+            10 - shadowHunter               [researches shadow banned accounts]
+            11 - geoLocater                 [attempts to grab user's location]
+            """)
+            print()
+            introduction()
+        if choice == 'socials':
+            socials()
+        if choice == 'username':
+            print("twosint~# The set username is -> ", username)
+            introduction()
+        if choice == 'run usernameSearch' or choice == 'run 0':
+            print()
+            usernameSearch()
+        if choice == 'run 1' or choice == 'run keyHunter':
+            print()
+            keyHunter()
+        if choice == 'run 2' or choice == 'run mailHunter':
+            print()
+            mailHunter()
+        if choice == 'run 3' or choice == 'run numHunter':
+            print()
+            numHunter()
+        if choice == 'run 4' or choice == 'run followHunter':
+            print()
+            followHunter()
+        if choice == 'run 5' or choice == 'run whoHunter':
+            print()
+            whoHunter()
+        if choice == 'run 6' or choice == 'run soloInvestigation':
+            print()
+            print("twosint~# Running the soloInvestigation module ... ")
+            soloInvestigation()
+        if choice == 'run 7' or choice == 'run nearVan':
+            print()
+            nearVan()
+        if choice == 'run 8' or choice == 'run link3r':
+            print()
+            link3r()
+        if choice == 'run 9' or choice == 'run cl0udchas3r':
+            print()
+            cl0udchas3r()
+            print()
+        if choice == 'run 10' or choice == 'run shadowHunter':
+            print()
+            shadowHunter()
+            print()
+        if choice == 'run 11' or choice == 'run geoLocater':
+            print()
+            geoLocater()
+            print()
+        if choice == 'info 0' or choice == 'info usernameSearch':
+            print()
+            infoUsernameSearch()
+            print()
+        if choice == 'info 1' or choice == 'info keyHunter':
+            print()
+            infoKeyHunter()
+            print()
+        if choice == 'info 2' or choice == 'info mailHunter':
+            print()
+            infoMailHunter()
+            print()
+        if choice == 'info 3' or choice == 'info numHunter':
+            print()
+            infoNumHunter()
+            print()
+        if choice == 'info 4' or choice == 'info followHunter':
+            print()
+            infoFollowHunter()
+            print()
+        if choice == 'info 5' or choice == 'info whoHunter':
+            print()
+            infoWhoHunter()
+            print()
+        if choice == 'info 6' or choice == 'info soloInvestigation':
+            print()
+            infoSoloInvestigation()
+            print()
+        if choice == 'info 7' or choice == 'info nearVan':
+            print()
+            infoNearVan()
+            print()
+        if choice == 'info 8' or choice == 'info link3r':
+            print()
+            infoLink3r()
+            print()
+        if choice == 'info 9' or choice == 'info cl0udchas3r':
+            print()
+            infoCl0udChas3r()
+            print()
+        if choice == 'info 10' or choice == 'info shadowHunter':
+            print()
+            infoShadowHunter()
+            print()
+        if choice == 'info 11' or choice == 'info geoLocater':
+            print()
+            infoGeoLocater()
+            print()
+        else:
+            introduction()
+    except KeyboardInterrupt as e:
+        print("twosint~# [!] Closing now ... ")
 
 introduction()
